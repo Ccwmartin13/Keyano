00e8adaf48832f8b659b7d4e69a83bc6ca734d5c
Chase Martin <ccwmartin13@gmail.com>
2017-03-02 20:01:21 -0600

    Added G Key
    Wurd

--- Keyano/Keyano/Keyano.cs
+++ Keyano/Keyano/Keyano.cs
@@ -36,11 +36,18 @@ namespace Keyano
                 //A.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_A.wav"));
                 A.Play();
             }
-            if (keyEventArgs.KeyCode == Keys.G)
+            if (keyEventArgs.KeyCode == Keys.G && keyEventArgs.KeyCode == Keys.Shift)
+            {
+                var GSharp = new MediaPlayer();
+                GSharp.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_G_sharp.wav"));
+                //G.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_G_sharp.wav"));
+                GSharp.Play();
+            }
+            if (keyEventArgs.KeyCode == Keys.G && keyEventArgs.KeyCode != Keys.Shift)
             {
                 var G = new MediaPlayer();
-                G.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_G_sharp.wav"));
-                //G.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_G_sharp.wav"));
+                G.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_G.wav"));
+                //G.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_G.wav"));
                 G.Play();
             }
         }
