8c767ee0550d6bb8dd197f9b6f27ed4b17ee341c
Chase Martin <ccwmartin13@gmail.com>
2017-03-04 16:28:09 -0600

    Added a few more keys.
    Not quite sure how to push this to master... I thought that this did
    that automatically.

--- Keyano/Keyano/Keyano.cs
+++ Keyano/Keyano/Keyano.cs
@@ -36,19 +36,34 @@ namespace Keyano
                 //A.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_A.wav"));
                 A.Play();
             }
-            if (keyEventArgs.KeyCode == Keys.G && keyEventArgs.KeyCode == Keys.Shift)
+            if (keyEventArgs.KeyCode == Keys.B)
             {
-                var GSharp = new MediaPlayer();
-                GSharp.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_G_sharp.wav"));
-                //G.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_G_sharp.wav"));
-                GSharp.Play();
+                var B = new MediaPlayer();
+                B.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_B.wav"));
+                //B.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_B.wav"));
+                B.Play();
             }
-            if (keyEventArgs.KeyCode == Keys.G && keyEventArgs.KeyCode != Keys.Shift)
+            if (keyEventArgs.KeyCode == Keys.G)
             {
                 var G = new MediaPlayer();
                 G.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_G.wav"));
                 //G.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_G.wav"));
                 G.Play();
+            }
+            if (keyEventArgs.KeyCode == Keys.C)
+            {
+                var CSharp = new MediaPlayer();
+                CSharp.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_C_sharp.wav"));
+                //CSharp.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_C_sharp.wav"));
+                CSharp.Play();
+            }
+            //Detecting both key click events is not working.. Not sure the reason.
+            if (keyEventArgs.KeyCode == Keys.G && keyEventArgs.KeyCode == Keys.Shift)
+            {
+                var GSharp = new MediaPlayer();
+                GSharp.Open(new Uri("C:\\Users\\cmart\\Desktop\\Playground\\Keyano\\Keyano\\Keyano\\Keyano Sounds\\piano_G_sharp.wav"));
+                //GSharp.Open(new Uri(@"C:\Users\Kody\Documents\Keyano\Keyano\Keyano\Keyano Sounds\piano_G_sharp.wav"));
+                GSharp.Play();
             }
         }
     }
